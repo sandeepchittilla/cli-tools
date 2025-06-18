36fc9f99549c2b92cad980947017d349d8085d5b
sandeepchittilla <sandeep.chittilla@gmail.com>
2025-06-18 17:59:37 +0100

    Add deps and update upload script

--- upload_to_bq.py
+++ upload_to_bq.py
@@ -3,31 +3,33 @@ import sys
 from google.cloud import bigquery
 
 if __name__=="__main__":
-    file_path = sys.argv[1] # input csv file
-    try:
-        assert file_path.endswith(".csv")
-    except Exception as ae:
-        print(f"Input file [{file_path}] is likely not a csv. Please provide csv file")
-        exit(1)
-      
-    table_id = sys.argv[2] # "your-project.your_dataset.your_table_name"
+##	print(sys.argv[1], sys.argv[2])
+##	exit(1)
+	file_path = sys.argv[1] # input csv file
+	try:
+		assert file_path.endswith(".csv")
+	except Exception as ae:
+		print(f"Input file [{file_path}] is likely not a csv. Please provide csv file")
+		exit(1)
+	  
+	table_id = sys.argv[2] # "your-project.your_dataset.your_table_name"
   
-    # Construct a BigQuery client object.
-    client = bigquery.Client()
+	# Construct a BigQuery client object.
+	client = bigquery.Client(project=table_id.split(".")[0])
   
-    job_config = bigquery.LoadJobConfig(
-        source_format=bigquery.SourceFormat.CSV, skip_leading_rows=1, autodetect=True,
-    )
-    
-    with open(file_path, "rb") as source_file:
-        job = client.load_table_from_file(source_file, table_id, job_config=job_config)
-    
-    job.result()  # Waits for the job to complete.
-    
-    table = client.get_table(table_id)  # Make an API request.
+	job_config = bigquery.LoadJobConfig(
+		source_format=bigquery.SourceFormat.CSV, skip_leading_rows=1, autodetect=True,
+	)
+	
+	with open(file_path, "rb") as source_file:
+		job = client.load_table_from_file(source_file, table_id, job_config=job_config)
+	
+	job.result()  # Waits for the job to complete.
+	
+	table = client.get_table(table_id)	# Make an API request.
   
-    print(
-        "Loaded {} rows and {} columns to {}".format(
-            table.num_rows, len(table.schema), table_id
-        )
-    )
+	print(
+		"Loaded {} rows and {} columns to {}".format(
+			table.num_rows, len(table.schema), table_id
+		)
+	)
